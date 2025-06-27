9210b47e2731728d483dd7f07aadae9515a70dcc
Ace <ace@localhost.localdomain>
2025-06-27 11:00:10 +0100

    removed unneeded constant

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,5 +1,4 @@
 const darkmodeToggle = document.querySelector('#darkmode-toggle');
-const Xlogo = document.querySelector('.sns-icon');
 
 darkmodeToggle.addEventListener('change', () => {
   if (darkmodeToggle.checked) {
